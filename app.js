a448d7b730f74e98db7a36be10f45fea8031fa46
Steve Busi <steve.busi@dudesolutions.com>
2017-02-05 16:42:26 -0500

    cleanup, messaging

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-require('shelljs/global');
-yaml = require('yamljs');
-//get arguments
-//var arg1 = '';
-var cwd = pwd().toString();
-yaml.load(cwd+ '/docker-compose.yml', function(result)
-{
-    var file = result;
-	var services = file.services;
-	Object.keys(services).map((e) => {
-		if (services[e].volumes !== undefined){
-			services[e].volumes.map((v)=> {
-				var dir = v.split(':')[0];
-				mkdir(dir);
-				console.log('created:' + dir);
-			});    
-		}
+var shell = require('shelljs');
+var yaml = require('yamljs');
+
+var cwd = shell.pwd().toString();
+var composefile = cwd + '/docker-compose.yml'
+
+if (!shell.test('-f', composefile)){
+	console.log('docker-compose.yml not found in current directory.');
+} else {
+	yaml.load(composefile, function(result)
+	{
+		var file = result;
+		var services = file.services;
+		Object.keys(services).map((e) => {
+			if (services[e].volumes !== undefined){
+				services[e].volumes.map((v)=> {
+					var dir = v.split(':')[0];
+					var msg = shell.mkdir(dir);
+					if (msg.stderr === null){
+						console.log('Created ' + dir);
+					}
+				});    
+			}
+		});
 	});
-});
+}
 
