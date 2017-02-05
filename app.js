1025cc8a30894146fefc352a08bbd534bc27df67
Steve Busi <steve.busi@dudesolutions.com>
2017-02-05 16:46:03 -0500

    fix tabs

--- app.js
+++ app.js
@@ -5,23 +5,23 @@ var cwd = shell.pwd().toString();
 var composefile = cwd + '/docker-compose.yml'
 
 if (!shell.test('-f', composefile)){
-	console.log('docker-compose.yml not found in current directory.');
+    console.log('docker-compose.yml not found in current directory.');
 } else {
-	yaml.load(composefile, function(result)
-	{
-		var file = result;
-		var services = file.services;
-		Object.keys(services).map((e) => {
-			if (services[e].volumes !== undefined){
-				services[e].volumes.map((v)=> {
-					var dir = v.split(':')[0];
-					var msg = shell.mkdir(dir);
-					if (msg.stderr === null){
-						console.log('Created ' + dir);
-					}
-				});    
-			}
-		});
-	});
+    yaml.load(composefile, function(result)
+    {
+        var file = result;
+        var services = file.services;
+        Object.keys(services).map((e) => {
+            if (services[e].volumes !== undefined){
+                services[e].volumes.map((v)=> {
+                    var dir = v.split(':')[0];
+                    var msg = shell.mkdir(dir);
+                    if (msg.stderr === null){
+                        console.log('Created ' + dir);
+                    }
+                });    
+            }
+        });
+    });
 }
 
